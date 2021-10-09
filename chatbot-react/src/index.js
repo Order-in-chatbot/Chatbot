f9a34f739385bb5b7b9da54569e1f47cf03fa006
OhHaneol <otyvs1999@gmail.com>
2021-10-10 06:28:25 +0900

    페이지 경로 수정

--- chatbot-react/src/index.js
+++ chatbot-react/src/index.js
@@ -1,8 +1,9 @@
+import React, {Suspense, lazy} from 'react';
 import ReactDOM from 'react-dom';
 import {Route, Switch,HashRouter as Router} from 'react-router-dom';
 const App=()=>{
-	const Main=lazy(()=>import('./main/main'));
-	const Chat=lazy(()=>import('./main/chat'));
+	const Main=lazy(()=>import('./main/jsx/main'));
+	const Chat=lazy(()=>import('./main/jsx/chat'));
 	return(
 	<Router>
 		<Suspense fallback={<div>Loading...</div>}>
