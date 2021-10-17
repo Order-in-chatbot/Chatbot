1a756513bf39f5aa25baee5c6b33bb780b2a814f
OhHaneol <otyvs1999@gmail.com>
2021-10-17 11:12:38 +0900

    api baseURL 수정중

--- chatbot-react/src/main/Api/api.jsx
+++ chatbot-react/src/main/Api/api.jsx
@@ -1,5 +1,7 @@
-
 const axios = require("axios");
+
+//TODO baseURL 수정 필요할 듯?
+axios.defaults.baseURL = 'http://aiopen.etri.re.kr:8000/'
 
 export const storeCode = {
     storeCodeApi: (storeNum) =>
@@ -9,11 +11,11 @@ export const storeCode = {
                 storeNum: storeNum
             },
         )
-        // .then(function (response) {
-        //   console.log(response);
-        // })
-        // .catch(function (error) {
-        //   console.log(error);
-        // })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
         ,
 };
