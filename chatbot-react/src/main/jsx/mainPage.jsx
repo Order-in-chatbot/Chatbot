38e6eefd6ab15d5b01234c76a28e086aea9b7005
OhHaneol <otyvs1999@gmail.com>
2021-10-17 11:13:05 +0900

    줄맞춤~

--- chatbot-react/src/main/jsx/mainPage.jsx
+++ chatbot-react/src/main/jsx/mainPage.jsx
@@ -14,8 +14,13 @@ const MainPage = ({ stCode, selectStore, handleStCode }) => {
                     <h1 style={{ fontSize: '7em' }}>오늘의<br/>식사를<br/>주문하세요</h1>
                 </div>
                 <Grid className="select">
-                    <TextField required name='stCode' value={stCode} onChange={handleStCode} id="filled-basic" label="키오스크 상단에 적힌 매장 코드를 입력해주세요." variant="filled" style={{ width: '100%' }} />
-                    <IconButton size="large" aria-label="이동" onClick={selectStore} style={{ width: '36px', marginLeft: '10px' }}><ChatIcon sx={{ fontSize: 36 }} /></IconButton>
+                    <TextField required name='stCode' value={stCode} onChange={handleStCode}
+                                id="filled-basic" label="키오스크 상단에 적힌 매장 코드를 입력해주세요."
+                                variant="filled" style={{ width: '100%' }} />
+                    <IconButton size="large" aria-label="이동" onClick={selectStore}
+                                style={{ width: '36px', marginLeft: '10px' }}>
+                        <ChatIcon sx={{ fontSize: 36 }} />
+                    </IconButton>
                 </Grid>
             </Grid>
 
